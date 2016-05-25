ab475d2696822eed7dc95b597c79f958eeacd690
jonah <jonahkh@uw.edu>
2016-05-24 19:37:30 -0700

    finished

--- app/src/main/java/jonahkh/tacoma/uw/edu/servicelab/MainActivity.java
+++ app/src/main/java/jonahkh/tacoma/uw/edu/servicelab/MainActivity.java
@@ -1,36 +1,40 @@
 package jonahkh.tacoma.uw.edu.servicelab;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public static final int MY_PERMISSIONS_REBOOT = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startService(new Intent(this, RSSService.class));
-        Button startButton = (Button) findViewById(R.id.start_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RSSService.setServiceAlarm(v.getContext(), true);
-            }
-        });
+        SharedPreferences sharedPreferences =
+                getSharedPreferences(getString(R.string.SHARED_PREFS), Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
 
         Button stopButton = (Button) findViewById(R.id.stop_button);
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RSSService.setServiceAlarm(v.getContext(), false);
+                editor.putBoolean(getString(R.string.ON), false);
+                editor.commit();
+
             }
         });
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_BOOT_COMPLETED)
                 != PackageManager.PERMISSION_GRANTED) {
 
@@ -39,5 +43,25 @@ public class MainActivity extends AppCompatActivity {
                     MY_PERMISSIONS_REBOOT);
         }
 
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RSSService.setServiceAlarm(v.getContext(), true);
+                editor.putBoolean(getString(R.string.ON), true);
+                editor.commit();
+
+            }
+
+        });
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String result = getIntent().getStringExtra(RSSService.FEED);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadData(result, "text/html", null);
+    }
+
 }
